2ef16e64b701b6ee1a794de7af6bc31932c8c2de
David LeGare <dlegare.1001@gmail.com>
2019-01-13 13:23:47 -0600

    Remove uuid_gen and move serde_dyn to the root

--- uuid_gen/src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate uuid;
-use std::mem::transmute;
-use uuid::Uuid;
-
-fn main() {
-    println!("{}", unsafe {
-        transmute::<[u8; 16], u128>(*Uuid::new_v4().as_bytes())
-    });
-}
